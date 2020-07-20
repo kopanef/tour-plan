b0236c206a40356acda76f04caa9a2c799fd1d55
kopanef <henrykopanev@gmail.com>
2020-07-20 23:12:10 +0300

    Evening commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var mySwiper = new Swiper('.swiper-container', {
 	loop: true,
 
 
-	// Navigation arrowss
+	// Navigation arrows
 	navigation: {
 		nextEl: '.slider-button--next',
 		prevEl: '.slider-button--prev',
