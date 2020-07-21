950ace9df2d1bdfd2a0c0714be8bd606441449aa
kopanef <henrykopanev@gmail.com>
2020-07-21 22:42:08 +0300

    add: yandex-maps integration to block .maps

--- js/main.js
+++ js/main.js
@@ -13,3 +13,29 @@ var mySwiper = new Swiper('.swiper-container', {
 	},
 
 })
+
+
+ymaps.ready(init);
+
+function init() {
+	// Создание карты.
+	var myMap = new ymaps.Map("map", {
+		// Координаты центра карты.
+		// Порядок по умолчанию: «широта, долгота».
+		// Чтобы не определять координаты центра карты вручную,
+		// воспользуйтесь инструментом Определение координат.
+		center: [55.774054, 37.651793],
+		// Уровень масштабирования. Допустимые значения:
+		// от 0 (весь мир) до 19.
+		zoom: 16
+	});
+	var myGeoObject = new ymaps.GeoObject({
+		geometry: {
+			type: "Point", // тип геометрии - точка
+			coordinates: [55.774054, 37.651793] // координаты точки
+		}
+	});
+
+	var myPlacemark = new ymaps.Placemark([55.774054, 37.651793]);
+	myMap.geoObjects.add(myPlacemark);
+}
