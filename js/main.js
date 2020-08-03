8ef8e069ee6bd2dcb4bf18afa4ba5d0de6dec558
kopanef <henrykopanev@gmail.com>
2020-08-03 14:35:37 +0800

    add: modal-windows for .booking and .rating

--- js/main.js
+++ js/main.js
@@ -1,81 +1,84 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-	// Optional parameters
-	loop: true,
-	keyboard: {
-		enabled: true,
-		onlyInViewport: false
-	},
+$(document).ready(function () {
+	var hotelSlider = new Swiper('.hotel-slider', {
+		// Optional parameters
+		loop: true,
+		keyboard: {
+			enabled: true,
+			onlyInViewport: false
+		},
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.hotel-slider__button--next',
-		prevEl: '.hotel-slider__button--prev',
-	},
+		// Navigation arrows
+		navigation: {
+			nextEl: '.hotel-slider__button--next',
+			prevEl: '.hotel-slider__button--prev',
+		},
 
-})
+	})
 
-var reviewsSlider = new Swiper('.reviews-slider', {
-	// Optional parameters
-	loop: true,
-	keyboard: {
-		enabled: true,
-		onlyInViewport: false
-	},
+	var reviewsSlider = new Swiper('.reviews-slider', {
+		// Optional parameters
+		loop: true,
+		keyboard: {
+			enabled: true,
+			onlyInViewport: false
+		},
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.reviews-slider__button--next',
-		prevEl: '.reviews-slider__button--prev',
-	},
+		// Navigation arrows
+		navigation: {
+			nextEl: '.reviews-slider__button--next',
+			prevEl: '.reviews-slider__button--prev',
+		},
 
-})
+	})
 
-var menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function () {
-	console.log('Клик по кнопке меню');
-	document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
-})
-
-
+	var menuButton = $('.menu-button');
+	menuButton.on('click', function () {
+		$('.navbar-bottom').toggleClass('navbar-bottom--visible')
+	})
 
 
+	ymaps.ready(init);
+
+	function init() {
+		// Создание карты.
+		var myMap = new ymaps.Map("map", {
+			// Координаты центра карты.
+			// Порядок по умолчанию: «широта, долгота».
+			// Чтобы не определять координаты центра карты вручную,
+			// воспользуйтесь инструментом Определение координат.
+			center: [55.774054, 37.651793],
+			// Уровень масштабирования. Допустимые значения:
+			// от 0 (весь мир) до 19.
+			zoom: 16
+		});
+		var myGeoObject = new ymaps.GeoObject({
+			geometry: {
+				type: "Point", // тип геометрии - точка
+				coordinates: [55.774054, 37.651793] // координаты точки
+			}
+		});
+
+		var myPlacemark = new ymaps.Placemark([55.774054, 37.651793]);
+		myMap.geoObjects.add(myPlacemark);
+	}
+
+	var modalButton = $("[data-toggle=modal]");
+	var closeModalButton = $(".modal__close");
+	modalButton.on("click", openModal);
+	closeModalButton.on('click', closeModal)
 
 
+	function openModal() {
+		var targetModal = $(this).attr('data-href');
+		$(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+		$(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-ymaps.ready(init);
-
-function init() {
-	// Создание карты.
-	var myMap = new ymaps.Map("map", {
-		// Координаты центра карты.
-		// Порядок по умолчанию: «широта, долгота».
-		// Чтобы не определять координаты центра карты вручную,
-		// воспользуйтесь инструментом Определение координат.
-		center: [55.774054, 37.651793],
-		// Уровень масштабирования. Допустимые значения:
-		// от 0 (весь мир) до 19.
-		zoom: 16
-	});
-	var myGeoObject = new ymaps.GeoObject({
-		geometry: {
-			type: "Point", // тип геометрии - точка
-			coordinates: [55.774054, 37.651793] // координаты точки
-		}
-	});
-
-	var myPlacemark = new ymaps.Placemark([55.774054, 37.651793]);
-	myMap.geoObjects.add(myPlacemark);
-}
+	function closeModal(event) {
+		event.preventDefault();
+		var modalOverlay = $(".modal__overlay");
+		var modalDialog = $(".modal__dialog");
+		modalOverlay.removeClass('modal__overlay--visible');
+		modalDialog.removeClass('modal__dialog--visible');
+	}
+});
