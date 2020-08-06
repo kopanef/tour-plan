7d2c98c8ef7417b87c5b2f35cd0fb78ba9aa840b
kopanef <henrykopanev@gmail.com>
2020-08-06 17:53:45 +0800

    add: validation for forms

--- js/main.js
+++ js/main.js
@@ -78,7 +78,27 @@ $(document).ready(function () {
 		event.preventDefault();
 		var modalOverlay = $(".modal__overlay");
 		var modalDialog = $(".modal__dialog");
-		modalOverlay.removeClass('modal__overlay--visible');
-		modalDialog.removeClass('modal__dialog--visible');
+		modalOverlay.removeClass("modal__overlay--visible");
+		modalDialog.removeClass("modal__dialog--visible");
 	}
+
+	// Обработка форм
+	$(".form").each(function () {
+		$(this).validate({
+			errorClass: "invalid",
+			messages: {
+				name: {
+					required: "Write your name",
+					minlength: "Name should contain more than one simbol",
+				},
+				email: {
+					required: "We need your email address to contact you",
+					email: "Your email address must be in the format of name@domain.com",
+				},
+				phone: {
+					required: "Write your phone",
+				},
+			},
+		});
+	});
 });
